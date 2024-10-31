e699c64370bcf18b5c4952d4bfbf57ae594cd75d
Artur Zimoń <zimart@wp.pl>
2024-10-31 12:21:46 +0000

    System generated commit, initial commit

---
+++ pages/Podsumowanie/jsobjects/Constants/Constants.js
@@ -0,0 +1,34 @@
+export default {
+
+	defaultLanguageKeys: {
+	
+		"Język": "Język",
+		"Ilość" : "Ilość",
+		"DODAJ STEROWANIE": "DODAJ STEROWANIE",
+		"Zmiana języka": "Zmiana języka",
+		"Szerokość":"Szerokość",
+		"Produkt":"Produkt",
+		"Wysokość":"Wysokość",
+		"Skrzynka":"Skrzynka",
+		"Cena bazowa systemu":"Cena bazowa systemu",
+		"Kolor":"Kolor",
+		"Waluta":"Waluta",
+	 	"Razem netto":"Razem netto",
+		"Cena netto":"Cena netto",
+		"Rabat":"Rabat",
+		"Kwota rabatu":"Kwota rabatu",
+		"Cena po rabacie":"Cena po rabacie",
+		"Suma VAT":"Suma Vat",
+		"Brutto":"Brutto",
+		"Podatek VAT":"Podatek VAT",
+		"Montaż":"Montaż",
+		"Pilot 1-kanałowy ręczny":"Pilot 1-kanałowy ręczny"
+	
+				
+	},
+	languageOptions: [
+		"Polski",
+		"Deutch",
+		"English"
+	]
+}
